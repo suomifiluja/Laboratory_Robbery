16d4c6fc8dfdafaf4914ad448173885d2e13df13
suomifiluja <80551518+suomifiluja@users.noreply.github.com>
2021-03-13 14:17:44 +0200

    Add files via upload

---
+++ esx_laboratory/esx_labra/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+Config.Locale = 'en'
+Config.NumberOfCopsRequired = 7
+
+Banks = {
+	["humane_labs"] = {
+		position = { ['x'] = 3536.17, ['y'] = 3660.11, ['z'] = 28.12 },
+		reward = math.random(20000,30000),
+		nameofbank = "Humane Labs",
+		lastrobbed = 5400
+	},
+}
---
+++ esx_laboratory/esx_labra/locales/en.lua
@@ -0,0 +1,23 @@
+Locales['en'] = {
+
+	['robbery_cancelled'] = 'the robbery will be cancelled, you will gain nothing.',
+	['robbery_successful'] = 'Ryosto onnistunut. Tienasit ~g~$.',
+	['bank_robbery'] = 'Laboratiorio',
+	['press_to_rob'] = 'Paina ~INPUT_CONTEXT~ ryostaaksesi ~b~',
+	['robbery_of'] = 'Ryostat Humane Labsia: ~r~',
+	['seconds_remaining'] = '~w~ sekunttia jaljella',
+	['robbery_cancelled_at'] = '~r~ Humane Labs ryosto peruttu: ~b~',
+	['robbery_has_cancelled'] = '~r~ Humane Labs ruosto peruttu: ~b~',
+	['already_robbed'] = 'Tama on jo ryostetty. Odota!',
+	['seconds'] = 'sekunttia.',
+	['rob_in_prog'] = '~r~ Ryosto menossa: ~b~',
+	['started_to_rob'] = 'Aloit ryostamaan ',
+	['do_not_move'] = ', Ala liiku!',
+	['alarm_triggered'] = 'Laukaisit halytyksen',
+	['hold_pos'] = 'Odota 10 minuuttia ja rahat ovat sinun!',
+	['robbery_complete'] = '~r~ Ryosto suoritettu.~s~ ~h~ Juokse!',
+	['robbery_complete_at'] = '~r~ Ryosto suoritettu: ~b~',
+	['min_two_police'] = 'tarvittava maara poliiseja: ',
+	['robbery_already'] = '~r~Ryosto jo menossa.',
+
+}
